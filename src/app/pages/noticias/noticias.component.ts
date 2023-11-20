c380e499d02f40db1991813a72e52e0726ad87b8
Jorge Willian Paez Nagakura <jorgewillianpaez@gmail.com>
2023-11-20 12:42:03 -0300

    Ajustes e alterações

--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
-  styleUrls: ['./noticias.component.scss']
+  styleUrls: ['./noticias.component.scss'],
+  providers: [MessageService],
 })
-export class NoticiasComponent {
-
-}
+export class NoticiasComponent {}
